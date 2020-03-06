1036883ee7373157f659a878d815199dd8b523c9
Andrew Philpott <andrewphilpott92@gmail.com>
2020-03-06 11:47:32 -0800

    Add code to pass test.

--- WordCounter/Models/WordCounter.cs
+++ WordCounter/Models/WordCounter.cs
@@ -15,7 +15,68 @@ namespace WordCounter.Models
 
     public static bool WordContainsOnlyLetterCharacters(string word)
     {
-      return false;
+      char[] characters = word.ToLower().ToCharArray();
+      foreach (char item in characters)
+      {
+        switch (item)
+        {
+          case 'a':
+            break;
+          case 'b':
+            break;
+          case 'c':
+            break;
+          case 'd':
+            break;
+          case 'e':
+            break;
+          case 'f':
+            break;
+          case 'g':
+            break;
+          case 'h':
+            break;
+          case 'i':
+            break;
+          case 'j':
+            break;
+          case 'k':
+            break;
+          case 'l':
+            break;
+          case 'm':
+            break;
+          case 'n':
+            break;
+          case 'o':
+            break;
+          case 'p':
+            break;
+          case 'q':
+            break;
+          case 'r':
+            break;
+          case 's':
+            break;
+          case 't':
+            break;
+          case 'u':
+            break;
+          case 'v':
+            break;
+          case 'w':
+            break;
+          case 'x':
+            break;
+          case 'y':
+            break;
+          case 'z':
+            break;
+          default:
+            return false;
+        }
+      }
+      return true;
     }
   }
 }
